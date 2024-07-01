378ffcb87a81c7f116335cb550532ea0bdd28f7b
Joaco182004 <joaquindiazchau@gmail.com>
2024-07-01 12:26:33 -0500

    Cambio de dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import {React,useEffect} from "react";
+import { useLocation } from 'react-router-dom';
 import {
   HomeIcon,
   ClipboardDocumentListIcon,
@@ -17,17 +18,25 @@ import {
 import "../pages/style.css"
 import Home from "./Home";
 export default function Dashboard() {
+  const location = useLocation();
+  
   return (
     <section className="bg-[rgb(244,244,244)] w-screen h-screen flex items-center">
       <nav className="bg-white h-[100%] w-[300px] flex flex-col">
         <img src="https://freepngimg.com/save/32196-pepsi-logo-transparent/686x670" className="h-[60px] w-[60px] ml-[10px] mt-[10px]"></img>
 
         <ul className="flex flex-col items-center w-full mt-5">
-          <li className="group hover:bg-[rgb(244,244,244)] hover:shadow-md cursor-pointer p-[10px] rounded-md flex items-center w-[90%] mb-2 hover:font-semibold">
-            <HomeIconLine className="size-6 text-[rgb(39,42,48)] group-hover:hidden" />
-            <HomeIcon className="size-6 text-[rgb(39,42,48)] hidden group-hover:block" />
-            <p className="font-montserrat ml-2 text-[rgb(39,42,48)]">Home</p>
-          </li>
+          
+            {location.pathname == "/dashboard" ? (
+            <li className="group bg-[rgb(244,244,244)] shadow-md cursor-pointer p-[10px] rounded-md flex items-center w-[90%] mb-2 font-semibold">
+              <HomeIcon className="size-6 text-[rgb(39,42,48)]  group-hover:block" />
+              <p className="font-montserrat ml-2 text-[rgb(39,42,48)]">Home</p>
+              </li>
+            ):(<li className="group hover:bg-[rgb(244,244,244)] hover:shadow-md cursor-pointer p-[10px] rounded-md flex items-center w-[90%] mb-2 hover:font-semibold"><HomeIconLine className="size-6 text-[rgb(39,42,48)] group-hover:hidden" />
+              <HomeIcon className="size-6 text-[rgb(39,42,48)] hidden group-hover:block" />
+              <p className="font-montserrat ml-2 text-[rgb(39,42,48)]">Home</p></li>)}
+            
+        
           <li className="group hover:bg-[rgb(244,244,244)] hover:shadow-md cursor-pointer p-[10px] rounded-md flex items-center w-[90%] mb-2 hover:font-semibold">
             <ClipboardDocumentListIconLine className="size-6 text-[rgb(39,42,48)] group-hover:hidden" />
             <ClipboardDocumentListIcon className="size-6 text-[rgb(39,42,48)] hidden group-hover:block" />
@@ -50,7 +59,7 @@ export default function Dashboard() {
           </li>
         </ul>
       </nav>
-      <div className="w-full h-full bg-red-500">
+      <div className="w-full h-full border-[rgb(244,244,244)] border-l-2">
         <div className="h-[80px] w-full bg-white">
 
         </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
-
+import "../pages/style.css"
 export default function Home() {
   return (
     <section className='w-full h-full'>
-        <h1 className='font-montserrat font-semibold mb-4 ml-4 pt-4 text-3xl'>Dashboard</h1>
+        <h1 className='font-montserrat font-semibold mb-[1.5rem] ml-[2rem] pt-[2rem] text-4xl'>Dashboard</h1>
+        <div className=' w-full cont-section-dash flex gap-4'>
+          <div className='bg-white w-[650px] h-[420px]  ml-[2rem] rounded-md flex flex-col items-center'>
+          <div className='flex w-[95%] mt-3 justify-start items-center'>
+          <div className='bg-blue-500 rounded-md w-3 h-8'></div>
+          <h2 className='font-montserrat font-semibold text-lg ml-2'>Vista General</h2>
+          </div>
+          <div className='w-[95%] flex bg-[rgb(244,244,244)] p-2 gap-4 mt-3 rounded-md'>
+                  <div className='w-1/2 h-32 bg-white rounded-md'>
 
+                  </div>
+                  <div className='w-1/2 h-32 [rgb(244,244,244)] rounded-md'>
+
+                  </div>
+              </div>
+          </div>
+          <div className='bg-white w-[30%] mr-4 mb-4 rounded-md'>
+             
+          </div>
+        </div>
+       
     </section>
   )
 }
