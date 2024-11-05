c3229d6bc17749c52ccdf9f0542f5520fb4b9286
Joaco182004 <joaquindiazchau@gmail.com>
2024-11-05 00:28:11 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'] // Dividir las bibliotecas de React en un chunk separado
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1500 // Ajustar el límite de tamaño de advertencia a 1000 kB o el valor que prefieras
+  }
 })
